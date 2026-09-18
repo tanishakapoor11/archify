ddc6ee8a824a2e2b9223cac82a52213f1664c686
Tanisha Kapoor <tanisha@jupitermeta.io>
2026-09-18 16:45:59 +0530

    Generate 3D design

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -46,7 +46,7 @@ export default function Home() {
     navigate(`/visualizer/${newId}`, {
       state: {
         initialImage: saved.sourceImage,
-        initialRendered: saved.renderedImage || null,
+        initialRender: saved.renderedImage || null,
         name,
       },
     });
